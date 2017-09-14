1b4d04233b52350106b25246b0b12f2969af94f5
utkarshmani1997 <utkarshmani1997@gmail.com>
2017-09-14 13:27:21 +0530

    Add rback setup to run in k8s

--- prometheus-monitoring/main.go
+++ prometheus-monitoring/main.go
@@ -5,54 +5,54 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 var (
-	// How often our /hello request durations fall into one of the defined buckets.
+	// How often our /latest/volume request durations fall into one of the defined buckets.
 	// We can use default buckets or set ones we are interested in.
-	duration = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "hello_request_duration_seconds",
-		Help:    "Histogram of the /hello request duration.",
-		Buckets: []float64{0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
-	})
+	volumeRequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "volume_request_duration_seconds",
+			Help:    "Histogram of the /latest/volumes request duration.",
+			Buckets: []float64{0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
+		},
+		[]string{"code", "method"},
+	)
 	// Counter vector to which we can attach labels. That creates many key-value
 	// label combinations. So in our case we count requests by status code separetly.
-	counter = prometheus.NewCounterVec(
+	volumeRequestCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "hello_requests_total",
-			Help: "Total number of /hello requests.",
+			Name: "volume_requests_total",
+			Help: "Total number of /latest/volumes requests.",
 		},
-		[]string{"status"},
+		[]string{"code", "method"},
 	)
 )
 
 // init registers Prometheus metrics.
 func init() {
-	prometheus.MustRegister(duration)
-	prometheus.MustRegister(counter)
+	prometheus.MustRegister(volumeRequestDuration)
+	prometheus.MustRegister(volumeRequestCounter)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	var status int
-
+	var code int
 	defer func(begun time.Time) {
-		duration.Observe(time.Since(begun).Seconds())
+		volumeRequestDuration.WithLabelValues(strconv.Itoa(code), r.Method).Observe(time.Since(begun).Seconds())
 
-		// hello_requests_total{status="200"} 2385
-		counter.With(prometheus.Labels{
-			"status": fmt.Sprint(status),
-		}).Inc()
+		// volume_requests_total{status="200"}
+		volumeRequestCounter.WithLabelValues(strconv.Itoa(code), r.Method).Inc()
 	}(time.Now())
-
-	status = 200
-	w.WriteHeader(status)
+	code = 200
+	w.WriteHeader(code)
 
 	fmt.Fprintf(w, "Hi there, the end point is :  %s !", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/latest/volumes", handler)
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
