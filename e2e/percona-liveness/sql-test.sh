3076b1da62c1f8ecb3937dbb4fabecc34e36f6ee
dargasudarshan <sudarshan.darga@cloudbyte.com>
2018-07-12 15:15:38 +0530

    Update sql-test.sh

--- e2e/percona-liveness/sql-test.sh
+++ e2e/percona-liveness/sql-test.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+echo -e "\nWaiting for mysql server to start accepting connections.."
+retries=10;wait_retry=30
+for i in `seq 1 $retries`; do
+  mysql -uroot -pk8sDem0 -e 'status' > /dev/null 2>&1
+  rc=$?
+  [ $rc -eq 0 ] && break
+  sleep $wait_retry
+done
 
+if [ $rc -ne 0 ];
+then
+  echo -e "\nFailed to connect to db server after trying for $(($retries * $wait_retry))s, exiting\n"
+  exit 1
+fi
 mysql -uroot -pk8sDem0 -e "CREATE DATABASE Inventory;"
 mysql -uroot -pk8sDem0 -e "CREATE TABLE Hardware (id INTEGER, name VARCHAR(20), owner VARCHAR(20),description VARCHAR(20));" Inventory
 mysql -uroot -pk8sDem0 -e "INSERT INTO Hardware (id, name, owner, description) values (1, "dellserver", "basavaraj", "controller");" Inventory
