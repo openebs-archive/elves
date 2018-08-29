ff27216b447a4a9d4efa9ae148244028e9e84117
dargasudarshan <sudarshan.darga@cloudbyte.com>
2018-08-29 15:34:39 +0530

    Update jupyter-loadgen.py

--- e2e/jupyter-loadger/jupyter-loadgen.py
+++ e2e/jupyter-loadger/jupyter-loadgen.py
@@ -5,7 +5,7 @@ while i < 1000:
     time.sleep(10)
     os.system("python generate_io.py")
     i += 1
-    os.command("sync;sync;sync")
+    os.system('sync;sync;sync')
     time.sleep(20)
     os.remove("file.txt")
 
